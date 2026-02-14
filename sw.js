0efd3a7edf64f407dfe781db41fae0854b60250d
boldprojekte <kontakt@bold-projekte.com>
2026-02-14 10:00:45 +0100

    Daily Limit, bessere Intervalle, Zufall-Modus

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'lernkarten-v3';
+const CACHE_NAME = 'lernkarten-v4';
 const ASSETS = ['./index.html', './cards.json', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', e => {
